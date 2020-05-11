7d0ba01a3f8348a16aef877bc53b8d8656d7eef4
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-12 05:24:58 +0900

    add puts method of user rest money

--- app/models/ticket_vending_machine.rb
+++ app/models/ticket_vending_machine.rb
@@ -6,4 +6,20 @@ class TicketVendingMachine
   def movies = (movies)
     @movies = movies
   end
+
+  def diplay_menu
+    puts "どの映画を見ますか？"
+    i = 0
+    self.movies.each do |movie|
+      puts "#{i} #{movie.title}: #{movie.fee}円 "
+      i += 1
+    end
+  end
+
+  def display_result(customer)
+    choosed_movie = self.movies[customer.choose_movie]
+    customer.money -= choosed_movie.fee
+    puts "#{choosed_movie.title}のチケット購入"
+    puts "#{customer.name}の所持金が#{cusomer.money}になりました" 
+  end
 end

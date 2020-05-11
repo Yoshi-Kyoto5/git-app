ba5fcd72d77e28c1ca8117c2b665f5114aee2541
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-12 04:34:45 +0900

    make instance in exec.rb

--- app/models/exec.rb
+++ app/models/exec.rb
@@ -3,3 +3,29 @@ require './movie'
 require './ticket'
 require './ticket_vending_machine'
 
+customer = Customer.new("kakashi", 10000)
+
+ticket_vending_machine = TicketVendingMachine.new
+
+one_piece = Movie.new(
+                      "one_piece",
+                      1000,
+                      Date.new(2020.3.1),
+                      Date.new(2020.5.1)
+          )
+
+naruto = Movie.new(
+                  "naruto",
+                  1000,
+                  Date.new(2020.3.1),
+                  Date.new(2020.5.1)
+        )
+
+surm_dank = Movie.new(
+                      "surm_dank",
+                      1000,
+                      Date.new(20203.1),
+                      Date.new(2020.5.1)
+          )
+
+TicketVendingMachine.movies = [one_piece, naruto, surm_dank]

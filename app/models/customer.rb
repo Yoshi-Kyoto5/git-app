22b4436a8d4ea55deb3d219145dd580d7b7ceb71
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-12 05:33:35 +0900

    refactoring

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,12 @@ class Customer
   def choose_movie
     gets.to_i
   end
+
+  def buy(ticket_vending_machine)
+    ticket_vending_machine.display.menu
+    choosed_movie = ticket_vending_machine.movies[choose_movie]
+    self.money -= choose_movie.fee
+    self.ticket = Ticket.new(choosed_movie)
+    puts "#{choosed_movie.title}のチケット購入"
+    puts "#{name}の所持金が#{money}円"
 end

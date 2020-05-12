eed49eb298c5c6291aa15c3f3a48a6c520a66d8e
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-13 08:53:27 +0900

    add how to refactoring in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.published.popular.limit(10)
   end
 
   # GET /articles/1

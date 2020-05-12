f33b17a6ad1ebb6dce0a2fec5ab00eb59cce5dc7
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-05-13 08:53:59 +0900

    add refactoring in views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,14 @@
 module ArticlesHelper
+  def current_user_has?(instance)
+    user_signed_in && current_user == instance.user
+  end
+
+  # def article_link(article)
+  #   # class_name = article.popular? ? "popular" : "normal"
+  #   if article.published?
+  #     content_tag :a, "詳細", class: class_name
+  #   else
+  #     content_tag :div, "閲覧不可", class: class_name
+  #   end
+  # end
 end
